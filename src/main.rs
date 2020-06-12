023cf28dd5c67e3b2f3a0fd072e0994a52ee83b6
Ash Grevelink <ashlink1111@gmail.com>
2020-06-11 21:48:23 -0400

    Add temperature conversions

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ fn main() {
     println!("Fib 4 is {}", fibonacci(4));    
     println!("Fib 5 is {}", fibonacci(5));
     println!("Fib 6 is {}", fibonacci(6));
-    println!("Fib 7 is {}", fibonacci(7));
+    println!("Fib 7 is {}", fibonacci(7));    
+    println!("37 celsius is {} F", c_to_f(37.0));    
+    println!("98.6 F is {} celsius", f_to_c(98.6));    
 }
 
 fn fibonacci(n: u32) -> u32 {
@@ -20,4 +22,12 @@ fn fibonacci(n: u32) -> u32 {
         return n;
     }
     return fibonacci(n-1) + fibonacci(n-2);
+}
+
+fn f_to_c (f: f32) -> f32 {
+    ((f - 32.0) * 5.0) / 9.0
+}
+
+fn c_to_f (c: f32) -> f32 {
+    ((c / 5.0) * 9.0) + 32.0
 }
